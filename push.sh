fc0e131f014b95fb4021bef7d23856d7ec32d0c8
swing <18845124018@163.com>
2021-09-22 15:27:36 +0800

    update

---
+++ push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git config --global user.name "swing"
+git config --global user.email 18845124018@163.com 
+git add .
+git commit -m "update"
+git push origin
+
+echo update successful
